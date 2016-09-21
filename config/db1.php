7fb091214f2c3d29517e6309acb5bc5d96cb7175
宋祎超 <songyichao74@gmail.com>
2016-09-21 15:55:41 +0800

    Delete db1.php

--- config/db1.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=10.10.75.208;dbname=12306',
-    'username' => 'root',
-    'password' => 'guest@mioji',
-    'charset' => 'utf8',
-];
