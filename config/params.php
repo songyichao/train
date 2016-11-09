933b9b6f5c3ad7e1e34570c9c3bcb613f64c0acb
songyichao <songyichao@mioji.com>
2016-11-09 10:49:29 +0800

    fix

---
+++ config/params.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+	'adminEmail' => 'admin@example.com',
+	'seat_type' => [
+		'商务座' => 'swz_num',
+		'特等座' => 'tz_num',
+		'一等座' => 'zy_num',
+		'二等座' => 'ze_num',
+		'高级软卧' => 'gr_num',
+		'软卧' => 'rw_num',
+		'硬卧' => 'yw_num',
+		'软座' => 'rz_num',
+		'硬座' => 'yz_num',
+		'无座' => 'wz_num',
+	],
+	'ali_dayu' =>[
+		'm_appkey' => '234637591', //你自己的阿里大鱼key
+		'm_secretKey' => '072d43cc9d63ca7756d3fb182e03267b1', //你自己的阿里大鱼key
+		'm_sign_name' => '有票了',
+		'm_template_code' => 'SMS_22910019',
+	],
+];
