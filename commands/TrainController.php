bb1404880cec975cb243ea3bdf208c44da0cf42b
songyichao <songyichao74@gmail.com>
2018-11-06 18:50:15 +0800

    fix  query

--- commands/TrainController.php
+++ commands/TrainController.php
@@ -14,97 +14,128 @@ use app\models\UserHope;
 use Ladybug\Dumper;
 use yii\console\Controller;
 use app\helps\Tools;
+use Yii;
 
 class TrainController extends Controller
 {
-	public function actionIndex()
-	{
-		$user_log = $user_id = [];
-		$url = 'https://kyfw.12306.cn/otn/leftTicket/query';
-		$user_help = (new UserHope())->getUserHope();
-		if (!empty($user_help)) {
-			foreach ($user_help as $item) {
-				$data = [
-					'leftTicketDTO.train_date' => date('Y-m-d', $item['go_time']),
-					'leftTicketDTO.from_station' => $item['stat_station'],
-					'leftTicketDTO.to_station' => $item['end_station'],
-					'purpose_codes' => 'ADULT',
-				];
-				$response = Tools::curl($url, $data);
-				if ($response !== false) {
-					$res = json_decode($response, true);
-					$can_buy = [];
-					$ladybug = new Dumper();
-					//					echo  $ladybug->dump($res);
-					//					exit;
-					foreach ($res['data'] as $re) {
-						if (!empty($item['train_no']) && $re['queryLeftNewDTO']['station_train_code'] === $item['train_no']
-							&& $re['queryLeftNewDTO']['canWebBuy'] === 'Y'
-						) {
-							if (!empty($item['seat_type']) &&
-								($re['queryLeftNewDTO'][$item['seat_type']] !== '无' || $re['queryLeftNewDTO'][$item['seat_type']] !== '--')
-							) {
-								$can_buy[] = $re['queryLeftNewDTO']['station_train_code'];
-							} else {
-								$can_buy[] = $re['queryLeftNewDTO']['station_train_code'];
-							}
-						}
-						if (!empty($item['seat_type']) &&
-							($re['queryLeftNewDTO'][$item['seat_type']] !== '无' || $re['queryLeftNewDTO'][$item['seat_type']] !== '--')
-						) {
-							$can_buy[] = $re['queryLeftNewDTO']['station_train_code'];
-						}
-						if (empty($item['seat_type']) && empty($item['train_no'])) {
-							
-							if ($re['queryLeftNewDTO']['canWebBuy'] === 'Y') {
-								$can_buy[] = $re['queryLeftNewDTO']['station_train_code'];
-							}
-						}
-					}
-					if (!empty($can_buy)) {
-						$call_res = Tools::callToUser($item['phone']);
-						$user_id[] = $item['uid'];
-						$user_log[] = [
-							'hid' => $item['hid'],
-							'train_no' => json_encode($can_buy),
-							'buy_status' => empty($can_buy) ? 0 : 1,
-							'call_status' => $call_res ? 1 : 0,
-							'ctime' => time(),
-						];
-						
-					}
-				}
-			}
-			if (!empty($user_id)) {
-				if ($call_res) {
-					(new UserHope())->changeStatus($user_id);
-				}
-				(new Log())->saveLog($user_log);
-			}
-		}
-	}
-	
-	public function actionStation()
-	{
-		$response = Tools::curl('https://kyfw.12306.cn/otn/resources/js/framework/station_name.js', ['station_version' => '1.8970']);
-		if ($response === false) {
-			saveStation();
-		} else {
-			$str = str_replace('var station_names =', '', $response);
-			$str = str_replace("'", '', $str);
-			$str = trim($str);
-			$arr = explode('@', $str);
-			unset($arr[0]);
-			$station = [];
-			foreach ($arr as $v) {
-				$stat = explode('|', $v);
-				$station[] = [
-					'name' => $stat[1],
-					'code' => $stat[2],
-				];
-			}
-			$res = (new StationCode())->saveCode($station);
-			return $res;
-		}
-	}
+    public function actionIndex()
+    {
+        $user_log  = $user_id = [];
+        $url       = 'https://kyfw.12306.cn/otn/leftTicket/query';
+        $user_help = (new UserHope())->getUserHope();
+        if (!empty($user_help)) {
+            foreach ($user_help as $item) {
+                $data = [
+                    'leftTicketDTO.train_date'   => date('Y-m-d', $item['go_time']),
+                    'leftTicketDTO.from_station' => $item['stat_station'],
+                    'leftTicketDTO.to_station'   => $item['end_station'],
+//            'leftTicketDTO.train_date'   => '2018-11-28',
+//            'leftTicketDTO.from_station' => 'BJP',
+//            'leftTicketDTO.to_station'   => 'HBB',
+                    'purpose_codes'              => 'ADULT',
+                ];
+
+                $response = Tools::curl($url, $data);
+                if ($response !== false) {
+                    $res     = json_decode($response, true);
+                    $can_buy = [];
+                    $ladybug = new Dumper();
+//
+//            echo $ladybug->dump($res['data']);
+//            exit;
+                    var_dump(Yii::$app->params['seat_type']);
+                    foreach ($res['data']['result'] as $re) {
+                        $re = explode('|', $re);
+
+                        if ($re[3] == 'Z203') {
+                            $ladybug->dump($re);
+                            exit;
+                        }
+                        $re_json = [
+                            'queryLeftNewDTO' => [
+                                'station_train_code' => $re[3],
+                                'canWebBuy'          => $re[11],
+                                'swz_num'            => $re[32],
+                                'tz_num'             => $re[32],//暂时没找
+                                'zy_num'             => $re[31],
+                                'ze_num'             => $re[30],
+                                'gr_num'             => $re[21],
+                                'rw_num'             => $re[23],
+                                'yw_num'             => $re[28],
+                                'rz_num'             => $re[24],
+                                'yz_num'             => $re[29],
+                                'wz_num'             => $re[26],
+                            ],
+                        ];
+
+                        if (!empty($item['train_no']) && $re_json['queryLeftNewDTO']['station_train_code'] === $item['train_no']
+                            && $re_json['queryLeftNewDTO']['canWebBuy'] === 'Y'
+                        ) {
+                            if (!empty($item['seat_type']) &&
+                                ($re_json['queryLeftNewDTO'][$item['seat_type']] !== '无' || $re_json['queryLeftNewDTO'][$item['seat_type']] !== '--')
+                            ) {
+                                $can_buy[] = $re_json['queryLeftNewDTO']['station_train_code'];
+                            } else {
+                                $can_buy[] = $re_json['queryLeftNewDTO']['station_train_code'];
+                            }
+                        }
+                        if (!empty($item['seat_type']) &&
+                            ($re_json['queryLeftNewDTO'][$item['seat_type']] !== '无' || $re_json['queryLeftNewDTO'][$item['seat_type']] !== '--')
+                        ) {
+                            $can_buy[] = $re_json['queryLeftNewDTO']['station_train_code'];
+                        }
+                        if (empty($item['seat_type']) && empty($item['train_no'])) {
+
+                            if ($re_json['queryLeftNewDTO']['canWebBuy'] === 'Y') {
+                                $can_buy[] = $re_json['queryLeftNewDTO']['station_train_code'];
+                            }
+                        }
+                    }
+                    if (!empty($can_buy)) {
+                        $call_res   = Tools::callToUser($item['phone']);
+                        $user_id[]  = $item['uid'];
+                        $user_log[] = [
+                            'hid'         => $item['hid'],
+                            'train_no'    => json_encode($can_buy),
+                            'buy_status'  => empty($can_buy) ? 0 : 1,
+                            'call_status' => $call_res ? 1 : 0,
+                            'ctime'       => time(),
+                        ];
+
+                    }
+                }
+            }
+            if (!empty($user_id)) {
+                if ($call_res) {
+                    (new UserHope())->changeStatus($user_id);
+                }
+                (new Log())->saveLog($user_log);
+            }
+        }
+    }
+
+    public function actionStation()
+    {
+        $response = Tools::curl('https://kyfw.12306.cn/otn/resources/js/framework/station_name.js', ['station_version' => '1.8970']);
+        if ($response === false) {
+            saveStation();
+        } else {
+            $str = str_replace('var station_names =', '', $response);
+            $str = str_replace("'", '', $str);
+            $str = trim($str);
+            $arr = explode('@', $str);
+            unset($arr[0]);
+            $station = [];
+            foreach ($arr as $v) {
+                $stat      = explode('|', $v);
+                $station[] = [
+                    'name' => $stat[1],
+                    'code' => $stat[2],
+                ];
+            }
+            $res = (new StationCode())->saveCode($station);
+
+            return $res;
+        }
+    }
 }
--- helps/Tools.php
+++ helps/Tools.php
@@ -6,6 +6,7 @@
  * Date: 2016/10/17
  * Time: 上午11:38
  */
+
 namespace app\helps;
 
 use yii\httpclient\Client;
@@ -18,73 +19,78 @@ use yii\taobao\TopSdk;
 
 class Tools
 {
-	public static function curl($url, $data, $method = 'get')
-	{
-		$client = new Client();
-		$response = $client->createRequest()
-			->setMethod($method)
-			->setOptions([
-				'timeout' => 600,
-				'userAgent' => method_exists(Yii::$app->request, 'getUserAgent') ? Yii::$app->request->getUserAgent() : null,
-			])
-			->setHeaders([])
-			->setFormat('urlencoded')
-			->setUrl($url)
-			->setData($data)
-			->send();
-		if ($response->statusCode === '200') {
-			return $response->content;
-		} else {
-			return false;
-		}
-	}
-	
-	public static function sendCode($phone, $code)
-	{
-		TopSdk::set();
-		$taobao = new Autoloader();
-		$c = new TopClient();
-		$c->appkey = Yii::$app->params['ali_dayu']['m_appkey'];
-		$c->secretKey = Yii::$app->params['ali_dayu']['m_secretKey'];
-		$req = new AlibabaAliqinFcSmsNumSendRequest();
-		$req->setExtend("123456");
-		$req->setSmsType("normal");
-		$req->setSmsFreeSignName(Yii::$app->params['ali_dayu']['m_sign_name']);
-		$req->setSmsParam("{\"code\":\"$code\"}");
-		$req->setRecNum($phone);
-		$req->setSmsTemplateCode(Yii::$app->params['ali_dayu']['m_template_code']);
-		$resp = json_decode(json_encode($c->execute($req)), true);
-		if (isset($resp['result']['err_code']) && $resp['result']['err_code'] === '0') {
-			return true;
-		}
-		return false;
-	}
-	
-	public static function callToUser($phone)
-	{
-		TopSdk::set();
-		$taobao = new Autoloader();
-		$c = new TopClient();
-		$c->appkey = Yii::$app->params['ali_dayu']['m_appkey'];
-		$c->secretKey = Yii::$app->params['ali_dayu']['m_secretKey'];
-		$req = new AlibabaAliqinFcTtsNumSinglecallRequest();
-		$req ->setTtsParam('');
-		$req ->setCalledNum($phone);
-		$req ->setCalledShowNum(Yii::$app->params['ali_dayu']['t_show_num']);
-		$req ->setTtsCode(Yii::$app->params['ali_dayu']['t_template_code']);
-		$resp = json_decode(json_encode($c->execute($req)), true);
-		if (isset($resp['result']['err_code']) && $resp['result']['err_code'] === '0') {
-			return true;
-		}
-		return false;
-	}
-	
-	public static function code()
-	{
-		$code = '';
-		for ($i = 0; $i < 6; $i++) {
-			$code .= rand(0, 9);
-		}
-		return $code;
-	}
+    protected static $ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36';
+
+    public static function curl($url, $data, $method = 'get')
+    {
+        $client   = new Client();
+        $response = $client->createRequest()
+            ->setMethod($method)
+            ->setOptions([
+                'timeout'   => 600,
+                'userAgent' => method_exists(Yii::$app->request, 'getUserAgent') ? Yii::$app->request->getUserAgent() : self::$ua,
+            ])
+            ->setHeaders([])
+            ->setFormat('urlencoded')
+            ->setUrl($url)
+            ->setData($data)
+            ->send();
+        if ($response->statusCode === '200') {
+            return $response->content;
+        } else {
+            return false;
+        }
+    }
+
+    public static function sendCode($phone, $code)
+    {
+        TopSdk::set();
+        $taobao       = new Autoloader();
+        $c            = new TopClient();
+        $c->appkey    = Yii::$app->params['ali_dayu']['m_appkey'];
+        $c->secretKey = Yii::$app->params['ali_dayu']['m_secretKey'];
+        $req          = new AlibabaAliqinFcSmsNumSendRequest();
+        $req->setExtend("123456");
+        $req->setSmsType("normal");
+        $req->setSmsFreeSignName(Yii::$app->params['ali_dayu']['m_sign_name']);
+        $req->setSmsParam("{\"code\":\"$code\"}");
+        $req->setRecNum($phone);
+        $req->setSmsTemplateCode(Yii::$app->params['ali_dayu']['m_template_code']);
+        $resp = json_decode(json_encode($c->execute($req)), true);
+        if (isset($resp['result']['err_code']) && $resp['result']['err_code'] === '0') {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static function callToUser($phone)
+    {
+        TopSdk::set();
+        $taobao       = new Autoloader();
+        $c            = new TopClient();
+        $c->appkey    = Yii::$app->params['ali_dayu']['m_appkey'];
+        $c->secretKey = Yii::$app->params['ali_dayu']['m_secretKey'];
+        $req          = new AlibabaAliqinFcTtsNumSinglecallRequest();
+        $req->setTtsParam('');
+        $req->setCalledNum($phone);
+        $req->setCalledShowNum(Yii::$app->params['ali_dayu']['t_show_num']);
+        $req->setTtsCode(Yii::$app->params['ali_dayu']['t_template_code']);
+        $resp = json_decode(json_encode($c->execute($req)), true);
+        if (isset($resp['result']['err_code']) && $resp['result']['err_code'] === '0') {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static function code()
+    {
+        $code = '';
+        for ($i = 0; $i < 6; $i++) {
+            $code .= rand(0, 9);
+        }
+
+        return $code;
+    }
 }
